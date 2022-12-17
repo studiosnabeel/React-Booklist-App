d8c15f71b2ddf747dfce3bb740675fb978eef3b2
Nabeel Akbar <studiosnabeel@gmail.com>
2022-12-17 18:18:56 +0500

    Add Navbar to Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Navbar from './NavBar';
 
 export default function Header() {
   return (
     <div>
       <h1>Nabeel</h1>
       <span>Bookstore</span>
+      <Navbar />
     </div>
   );
 }
---
+++ src/components/NavBar.js
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+import { NavLink } from 'react-router-dom';
+
+function Navbar() {
+  const [active, setActive] = useState('Home');
+  const handleChange = (e) => {
+    setActive(e.target.innerHTML);
+  };
+
+  return (
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <NavLink
+              to="/"
+              onClick={handleChange}
+              className={active === 'Home' ? 'active' : ''}
+            >
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/categories" onClick={handleChange}>
+              Categories
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+}
+
+export default Navbar;
