44f3e60dc6f04af9a403529dc999ad290ebc5f66
Nabeel Akbar <studiosnabeel@gmail.com>
2022-12-17 13:17:33 +0500

    Create Header component and route it

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { Route, Routes } from 'react-router-dom';
 import Books from './components/Books';
 import Categories from './components/Categories';
+import Header from './components/Header';
 
 function App() {
   return (
     <>
       <div>
+        <Header />
         <Routes>
           <Route exact path="/" element={<Books />} />
           <Route
