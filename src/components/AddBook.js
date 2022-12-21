2d7b1b91b4888403070cefd84d9fc18cc023f1ac
Nabeel Akbar <studiosnabeel@gmail.com>
2022-12-21 14:14:49 +0500

    Modify AddBook

--- src/components/AddBook.js
+++ src/components/AddBook.js
@@ -1,12 +1,50 @@
-const AddBook = () => (
-  <div className="form">
-    <h3 className="add-title">ADD NEW BOOK</h3>
-    <form>
-      <input type="text" placeholder="Book Title" />
-      <input type="text" name="author" placeholder="Author" />
-      <button type="submit"> Add Book</button>
-    </form>
-  </div>
-);
+import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { v4 as uuidv4 } from 'uuid';
 
+import { addBook } from '../redux/books/books';
+
+const AddBook = () => {
+  const [book, setBook] = useState({});
+
+  const dispatch = useDispatch();
+
+  const inputValue = (element) => {
+    setBook({
+      ...book,
+      id: uuidv4(),
+      [element.target.name]: element.target.value,
+    });
+  };
+
+  return (
+    <div className="form">
+      <h3 className="add-title">ADD NEW BOOK</h3>
+      <form
+        action="/action_page.php"
+        onSubmit={(e) => {
+          e.preventDefault();
+          dispatch(addBook(book));
+          e.target.reset();
+        }}
+      >
+        <input
+          name="title"
+          type="text"
+          placeholder="Book Title"
+          onChange={(e) => inputValue(e)}
+          required
+        />
+        <input
+          type="text"
+          name="author"
+          placeholder="Author"
+          onChange={(e) => inputValue(e)}
+          required
+        />
+        <button type="submit"> Add Book</button>
+      </form>
+    </div>
+  );
+};
 export default AddBook;
